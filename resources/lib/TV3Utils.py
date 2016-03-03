feeec984b910d64367814802a562bd9d482dc0b8
jqandreu <msintes35@gmail.com>
2016-03-03 14:34:06 +0100

    Improved efficiency listing videos. Fixed website changes

---
+++ resources/lib/TV3Utils.py
@@ -0,0 +1,17 @@
+'''
+Created on 10 febr. 2016
+
+@author: Macia
+'''
+
+class TV3Utils(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
