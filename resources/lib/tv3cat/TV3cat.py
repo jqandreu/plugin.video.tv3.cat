feb0fa669ae68337403807d2e66cbb47efb599f5
Lluís Batlle i Rossell <viric@viric.name>
2023-01-21 10:15:27 +0100

    Fer funcionar Programes/
    El detectiu conan no anava

--- resources/lib/tv3cat/TV3cat.py
+++ resources/lib/tv3cat/TV3cat.py
@@ -476,11 +476,11 @@ class TV3cat(object):
                                 if urlProg == Urls.urlApm or urlProg == Urls.urlZonaZaping:
                                     url_final = urlProg + 'clips/'
 
-                                elif '/sx3/' in url or '/super3/' in url:
-                                    if 'https:' not in url:
-                                        url_final = 'https:' + url + 'videos'
-                                    else:
-                                        url_final = url
+#                                elif '/sx3/' in url or '/super3/' in url:
+#                                    if 'https:' not in url:
+#                                        url_final = 'https:' + url + 'videos'
+#                                    else:
+#                                        url_final = url
 
                                 else:
                                     match = re.compile(
